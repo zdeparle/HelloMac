6e02af5c886505280d3b9892e98e3c7d9f508be4
Zack <33883418+zdeparle@users.noreply.github.com>
2025-09-02 21:47:00 -0700

    k

--- HelloMac/ContentView.swift
+++ HelloMac/ContentView.swift
@@ -8,12 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var count: Int = 0
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+                .font(.title2)
+                .padding(.bottom, 8)
+
+            Text("Count: \(count)")
+                .font(.headline)
+                .padding(.bottom, 4)
+
+            Button(action: { count += 1 }) {
+                Text("Increment")
+            }
         }
         .padding()
     }
